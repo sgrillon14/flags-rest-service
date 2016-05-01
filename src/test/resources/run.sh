6fdc0f8180ba7408b33117f5a945aa11fb0cf5c6
sgrillon <sgrillon@hotmail.fr>
2016-05-01 20:23:02 +0200

    Add travis script

---
+++ src/test/resources/run.sh
@@ -0,0 +1,46 @@
+cd $(dirname $0)
+
+mvn clean compile
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew compileJava
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+mvn clean package
+java -jar target/flags-rest-service-0.1.0.jar &
+PID=$!
+sleep 10
+curl -s http://localhost:8084/flags > target/actual.json
+kill -9 $PID
+
+echo "Let's look at the actual results: `cat target/actual.json`"
+echo "And compare it to: `cat ../test/expected.json`"
+
+if diff -w /src/test/resources/expected.json target/actual.json
+    then
+        echo SUCCESS
+        let ret=0
+    else
+        echo FAIL
+        let ret=255
+        exit $ret
+fi
+
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+
+rm -rf build
+exit
