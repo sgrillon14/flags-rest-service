9ead4b9fab2af439b18a2a3378ff099bfd4c6fb0
sgrillon <sgrillon@hotmail.fr>
2016-05-02 23:10:05 +0200

    test application.yml

--- project/src/main/java/hello/Application.java
+++ project/src/main/java/hello/Application.java
@@ -2,12 +2,14 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@ConfigurationProperties(locations="classpath:application.yml")
 public class Application {
 
     public static void main(String[] args) {
