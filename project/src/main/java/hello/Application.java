094614d425bc3b74fb87aab25232fdd4fca92f9e
sgrillon <sgrillon@hotmail.fr>
2016-05-02 23:32:32 +0200

    test conf

--- project/src/main/java/hello/Application.java
+++ project/src/main/java/hello/Application.java
@@ -1,15 +1,13 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
