1407f450692d166a6389b58f59b3cabab269e4fa
sgrillon14 <sgrillon@hotmail.fr>
2016-05-03 09:18:19 +0200

    delete CORS

--- project/src/main/java/hello/Application.java
+++ project/src/main/java/hello/Application.java
@@ -14,14 +14,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
-            }
-        };
-    }
-
 }
--- project/src/main/java/hello/GreetingController.java
+++ project/src/main/java/hello/GreetingController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class GreetingController {
@@ -14,15 +13,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @CrossOrigin(origins = "http://localhost:9000")
     @RequestMapping("/greeting")
     public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
-
-    @RequestMapping("/greeting-javaconfig")
-    public @ResponseBody Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
         System.out.println("==== in greeting ====");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
