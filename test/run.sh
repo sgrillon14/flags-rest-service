efdaf98b8a6e8b745d970e724dbfbe961f4c8a47
sgrillon <sgrillon@hotmail.fr>
2016-05-20 22:33:39 +0200

    Change version of jar to 0.1.1

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,7 @@
 cd $(dirname $0)
 cd ../project
 mvn clean package
-java -jar target/flags-rest-service-0.1.0.jar &
+java -jar target/flags-rest-service-0.1.1.jar &
 PID=$!
 sleep 15
 curl -s http://localhost:8084/flags/api/countries > target/actual_countries.json
