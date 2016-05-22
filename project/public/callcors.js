e1ffebd1e36df094406b23ffb096f973f2700764
sgrillon <sgrillon@hotmail.fr>
2016-05-22 23:17:49 +0200

    clean of code after add CORS sample

--- project/public/callcors.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8084/flags/FR"
-    }).then(function(data, status, jqxhr) {
-       $('.flags-id').append(data.num);
-       $('.flags-content').append(data.alpha2);
-       console.log(jqxhr);
-    });
-});
