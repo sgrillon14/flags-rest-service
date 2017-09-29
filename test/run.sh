9c9ac81c2e875ee7b285965d4165b4153142cea0
Stéphane GRILLON <sgrillon14@users.noreply.github.com>
2017-09-29 18:08:47 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -5,7 +5,19 @@ java -jar target/flags-rest-service-3.0.0-SNAPSHOT.jar &
 PID=$!
 sleep 15
 
-curl -u admin:secret -s http://localhost:8088/health > target/health.json
+curl -u admin:secret -s http://localhost:8088/health > target/actual_health.json
+echo "Let's look at the actual health: `cat target/actual_health.json`"
+echo "And compare it to: `cat ../test/health.json`"
+if diff -w ../test/health.json target/actual_health.json
+    then
+        echo SUCCESS
+        let ret=0
+    else
+        echo FAIL
+        let ret=255
+        exit $ret
+fi
+
 
 curl -s http://localhost:8084/flags/api/countries > target/actual_countries.json
 curl -s http://localhost:8084/flags/api/countries?lang=en > target/actual_countries_EN.json
